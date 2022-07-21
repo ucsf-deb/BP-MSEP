234d7bda408ab3d562776848059880ef5f3c3d97
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2022-07-21 16:24:29 -0700

    Add tests for non-standard Normal  *  Executing task: C:\Programs\Julia-1.8.0-rc3\bin\julia.exe --color=yes --project=c:\Users\rdboylan\Documents\BP\MSEP c:\Users\rdboylan\Documents\BP\MSEP\test\runtests.jl
    Test Summary:           | Pass  Total  Time
    MSEP.jl standard normal |    3      3  9.3s
    Test Summary:      | Pass  Total  Time
    MSEP Normal(-1, 2) |    3      3  0.3s

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using MSEP
 using Test
 using FastGaussQuadrature
 
-@testset "MSEP.jl" begin
+@testset "MSEP.jl standard normal" begin
     ngh = MSEP.NormalGH(5)
     f1(x)=1.0
     @test ngh(f1) ≈ (1.0)
@@ -10,4 +10,14 @@ using FastGaussQuadrature
     @test ngh(f2) ≈ (0.0) atol=1e-14
     f3(x)=x^2
     @test ngh(f3) ≈ (1.0) 
+end
+
+@testset "MSEP Normal(-1, 2)" begin
+    ngh = MSEP.NormalGH(5, μ=-1, σ=2)
+    f1(x)=1.0
+    @test ngh(f1) ≈ (1.0)
+    f2(x)=x
+    @test ngh(f2) ≈ (-1.0) atol=1e-14
+    f3(x)=(x+1.0)^2
+    @test ngh(f3) ≈ (4.0) rtol=1e-14
 end

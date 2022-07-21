e9cbc39b0f7a5edea73060f3bbfb21db995fd7c2
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2022-07-21 16:15:19 -0700

    First working quadrature, with tests

--- src/MSEP.jl
+++ src/MSEP.jl
@@ -1,5 +1,37 @@
 module MSEP
+using FastGaussQuadrature
+using LinearAlgebra
 
-# Write your package code here.
+"""
+Use Gauss-Hermite quadrature to evaluate a function.
+Construct it by giving the number of points to evaluate and,
+optionally, the mean and standard deviation of the distribution.
+
+Thereafter you can treat the object as a function and call it on 
+a function f that takes a single argument x.
+
+The result is the f integrated over the normal distribution.
+"""
+struct NormalGH
+    npoints
+    μ
+    σ
+    w
+    x
+end
+
+"constructor"
+function NormalGH(npoints; μ=0.0, σ=1.0)
+    x, w = gausshermite(npoints)
+    return NormalGH(npoints, μ, σ, w ./ √π, x)
+end
+
+"""evaluate the integral, treating `NormalGH` instance as a function
+Integrate with respect to normal density.
+"""
+function (p::NormalGH)(f)
+    return dot(p.w, f.(p.μ.+√2*p.σ*p.x))  
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using MSEP
 using Test
+using FastGaussQuadrature
 
 @testset "MSEP.jl" begin
-    # Write your tests here.
+    ngh = MSEP.NormalGH(5)
+    f1(x)=1.0
+    @test ngh(f1) ≈ (1.0)
+    f2(x)=x
+    @test ngh(f2) ≈ (0.0) atol=1e-14
+    f3(x)=x^2
+    @test ngh(f3) ≈ (1.0) 
 end

f23b44790880fc8d2568f305e5593cf042b8b33e
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2022-07-22 15:03:47 -0700

    Evaluate the conditional density.  Add packages and exports. 1. Add Distributions, StatsFuns and Gadfly (plotting).  Esp the latter may not be necessary in the end, but for now I want them. 2. Get some constants from StatsFuns (yes, it has them despite the name) instead of computation. 3. Create condzy1 for the conditional likelihood of a special, but extreme, case. 4. started exports from MSEP.  A bit unclear what I'm exporting.   See comments in code.
    Goal was to see where the thick part of the conditional distribution was in
    the worst case.  Used
    plot(z->condzy1(z, -4), -5, 5, Scale.x_continuous(minticks=5), Guide.xticks(ticks= -5:1:5))
    to look at results.  Max was around 1

--- src/MSEP.jl
+++ src/MSEP.jl
@@ -1,6 +1,14 @@
 module MSEP
+using Distributions
 using FastGaussQuadrature
 using LinearAlgebra
+using StatsFuns
+
+# NormalGH is a struct and function name.  Not sure what the next line does.
+export NormalGH
+# and then there's the function associate with NormalGH instances
+
+export condzy1
 
 """
 Use Gauss-Hermite quadrature to evaluate a function.
@@ -23,15 +31,23 @@ end
 "constructor"
 function NormalGH(npoints; μ=0.0, σ=1.0)
     x, w = gausshermite(npoints)
-    return NormalGH(npoints, μ, σ, w ./ √π, x)
+    return NormalGH(npoints, μ, σ, w ./ sqrtπ, x)
 end
 
 """evaluate the integral, treating `NormalGH` instance as a function
 Integrate with respect to normal density.
 """
 function (p::NormalGH)(f)
-    return dot(p.w, f.(p.μ.+√2*p.σ*p.x))  
+    return dot(p.w, f.(p.μ.+sqrt2*p.σ*p.x))  
 end
 
+"""
+conditional likelihood z|Y=1
+for a single outcome with constant term k
+The variance of the random effects is 1.
+"""
+function condzy1(z, k)
+    return logistic(k+z)*pdf(Normal(), z)
+end
 
 end

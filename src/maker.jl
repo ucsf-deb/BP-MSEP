23e130c44e5bc8fc2b264b78039042054942fb65
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2022-09-15 12:16:44 -0700

    Add \sigma parameter to control sd of random effects. Also flesh out comments, esp about return values. Switched from use randn from Julia's random package to Distributions' Normal. The immediate motivation was handling non-standard normal, but this also means the Normals and Bernoulli are on the same package, and should be be controlled by its seed. It was unclear before if setting the seed for one would do so for the other.

--- src/maker.jl
+++ src/maker.jl
@@ -4,21 +4,36 @@ using MixedModels
 using StatsFuns
 
 """
-    maker(nclusters=3, nclustersize=4, k=-2)
+    maker(nclusters=3, nclustersize=4, k=-2.0, σ=1.0)
 
 Return a dataset from a simple mixed model with the indicated sizes and variables
-cluster random effects are N(0, 1)
+cluster random effects are N(0, σ)
 Y binary outcome
-z usually unknown random effect
 cid cluster id
 sid subject id (unique across all clusters)
+The id's are currently integers, but perhaps should be Categorical.
+MixedModels formula syntax says cluster ids should be Categorical,
+but in their examples it's actually String.
+
+All the following are usually unobserved
+z true random effect (constant within cluster)
+η true value on the linear scale
+p true probability of success
+
 Output has one row per subject in anticipation of subject level variables
 """
-function maker(nclusters=3, nclustersize=4, k=-2)
-    zcluster = randn(nclusters)
+function maker(nclusters=3, nclustersize=4, k=-2.0, σ=1.0)
+    zcluster = rand(Normal(0.0, σ), nclusters)
     df = DataFrame(cid=repeat(1:nclusters, inner=nclustersize),
         sid=1:(nclusters*nclustersize),
         z=repeat(zcluster, inner=nclustersize))
+    # The remaining elements depend on all previous ones,
+    # and DataFrames doesn't seem good at that.  It doesn't work
+    # in transform.
+
+    # Since every member of the cluster is identical, the following
+    # procedure is a bit inefficient.  But in the future there will be 
+    # individual covariates, and this is the right flow for that.
     df.η = k .+ df.z
     df.p = logistic.(df.η)
     df.Y = rand.(Bernoulli.(df.p))

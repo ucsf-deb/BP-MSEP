d74bda6273976230355931bd89d7214da727a671
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2022-09-15 17:16:05 -0700

    Create MultiLevel type to hold cluster and individual results. Motivation was realizing we where going to get more cluster-level results and needed somewhere to put them.

--- src/maker.jl
+++ src/maker.jl
@@ -4,10 +4,24 @@ using MixedModels
 using StatsFuns
 
 """
+MultiLevel holds 2 DataFrames, one with individuals
+as rows as one with clusters as row.
+
+The cid's, cluster ids, should be the same for both.
+
+Although the structure is read-only, expected use is that 
+columns will be added to the contained data, esp for clusters.
+"""
+struct MultiLevel
+    individuals::DataFrame
+    clusters::DataFrame
+end
+
+"""
     maker(nclusters=3, nclustersize=4, k=-2.0, σ=1.0)
 
-Return a dataset from a simple mixed model with the indicated sizes and variables
-cluster random effects are N(0, σ)
+Return a dataset from a simple mixed model with the indicated sizes and variables.
+cluster random effects are N(0, σ) and the only fixed effect is the intercept k.
 Y binary outcome
 cid cluster id
 sid subject id (unique across all clusters)
@@ -22,9 +36,17 @@ p true probability of success
 
 Output has one row per subject in anticipation of subject level variables
 """
-function maker(nclusters=3, nclustersize=4, k=-2.0, σ=1.0)
+function maker(nclusters=3, nclustersize=4, k=-2.0, σ=1.0)::MultiLevel
     zcluster = rand(Normal(0.0, σ), nclusters)
-    df = DataFrame(cid=repeat(1:nclusters, inner=nclustersize),
+    clusters = DataFrame(z=zcluster, cid=1:nclusters, n=nclustersize)
+    # use clusters.cid is an attempt to preserve whatever
+    # scheme I come up with, e.g., still works if I change
+    # Categorical variables for IDs.
+
+    # if we have variable cluster sizes, get individual cids with
+    # cid = cat(map((id, n)->repeat([id], n), clusters.cid, clusters.n))
+    # and similarly with z
+    df = DataFrame(cid=repeat(clusters.cid, inner=nclustersize),
         sid=1:(nclusters*nclustersize),
         z=repeat(zcluster, inner=nclustersize))
     # The remaining elements depend on all previous ones,
@@ -37,7 +59,7 @@ function maker(nclusters=3, nclustersize=4, k=-2.0, σ=1.0)
     df.η = k .+ df.z
     df.p = logistic.(df.η)
     df.Y = rand.(Bernoulli.(df.p))
-    return df
+    return MultiLevel(df, clusters)
 end
 df = maker()
 df

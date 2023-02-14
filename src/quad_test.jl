0c2ba2279a3500db264a46b946d681de23e45220
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2023-02-14 13:08:25 -0800

    Get a grid of quadrature results as requested by Chuck. Results in the csv, distributed.

--- src/quad_test.jl
+++ src/quad_test.jl
@@ -50,6 +50,7 @@ function quad_test(df::DataFrame; σ=1.0, orders=3:15)
     return r
 end
 
+#=
 testdat = one_of_each(nClusterSize=7)
 println("ClusterSize = 7. k = -1.0.  Best Predictors for logistic.")
 println("σ=1.0 results follow:")
@@ -58,3 +59,35 @@ r1 =quad_test(testdat, σ=1.0, orders=orders)
 println()
 println("σ=1.25 results follow:")
 r2 =quad_test(testdat, σ=1.25, orders=orders)
+=#
+
+"""
+Ross- could you send me the values you are getting for numerical quadrature for z_SQ for the logistic model?  I want to check a Gauss-Hermite quadrature routine I have developed before I use it for further investigations.
+
+Could you do clusters of size 5 and 7, sigma of 0.5 and 1, mu=-1 and 0, lambda=0.2 and 0.4?  So 16 scenarios in all.  And give me the predicted values for 0 out of 5, 1 out of 5, 2 out of 5, etc. 
+
+"""
+function quad2(ostr; clusterSizes=[5, 7], sigmas=[0.5, 1.0], μs=[-1.0, 0.0], λs=[0.2, 0.4])
+    for clusterSize in clusterSizes
+        df = one_of_each(nClusterSize = clusterSize)
+        for λ in λs, k in μs, σ in sigmas
+            ev = LogisticSimpleEvaluator(λ, k, σ)
+            wa = MSEP.WorkArea(df, ev)
+            for iCluster in 0:clusterSize
+                wa.i_start = 1 + clusterSize*iCluster
+                wa.i_end = clusterSize*(iCluster+1)
+                wa.i_cluster = iCluster+1
+                zBP = zsimp(ev, wa)
+                zSQ = zhat(ev, wa)
+                write(ostr, "$(clusterSize),$(σ),$(k),$(λ),$(iCluster),$(zBP),$(zSQ)\n")
+            end
+        end
+    end
+end
+
+ofile = open("quad.csv", "w")
+write(ofile, "Quadrature Results for zSQ from MSEP\n")
+write(ofile,"From quad_test.jl:quad2() on $(now())\n")
+write(ofile, "clusterSize,sigma,mu,lambda,sumY,zBP,zSQ\n")
+quad2(ofile)
+close(ofile)

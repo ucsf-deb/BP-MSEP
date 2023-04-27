2c37494c1458fd131b6957b754dba323a4ac5d10
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2023-04-27 14:51:05 -0700

    test6 now sensitive to # of threads. Because every task that is spawned changes the parent RNG state. PS C:\Users\rdboylan\Documents\BP\MSEP> julia --project -t16 -- src\test6.jl [-0.6096911806529702, -0.4542257247591477, 0.16528763905031515, -0.12062756001589506, 0.8968783357334372, 0.5060655128978275, -0.34218046669006463, -0.3526521145673422, 0.4518655487713241, -1.1686014861086333] PS C:\Users\rdboylan\Documents\BP\MSEP> julia --project  -- src\test6.jl [0.40401615236372684, -2.4530141213201877, 0.7609440796042747, 0.4931999850247413, -0.4117305272372651, 0.8822169514594856, -0.07503118310039182, 0.22225124841559304, 0.5964174113780768, 0.288255723358961] PS C:\Users\rdboylan\Documents\BP\MSEP> julia --project -t1  -- src\test6.jl [0.40401615236372684, -2.4530141213201877, 0.7609440796042747, 0.4931999850247413, -0.4117305272372651, 0.8822169514594856, -0.07503118310039182, 0.22225124841559304, 0.5964174113780768, 0.288255723358961] PS C:\Users\rdboylan\Documents\BP\MSEP> julia --project -t2  -- src\test6.jl [-0.4117305272372651, 0.8822169514594856, -0.07503118310039182, 0.22225124841559304, 0.12712738849715016, -0.5346188720010416, -1.8661491365928784, 0.466611932906786, -1.0209655744896966, -0.24667618682784703] PS C:\Users\rdboylan\Documents\BP\MSEP> julia --project -t3  -- src\test6.jl [1.3291320383393166, -0.5346188720010416, -1.8661491365928784, 0.466611932906786, -1.1895973555170667, 0.4870907937466442, 1.008962967659328, 1.3133320560535473, -1.1018886311472877, 1.5624017007565159]
    Note absence of -t argument and -t1 give same results.
    
    However, the first simulated dataset in the original bigsim4 seems to vary with number of threads, even though no spawns happen til after it is generated....

--- src/test6.jl
+++ src/test6.jl
@@ -1,3 +1,11 @@
 using Random
+
 Random.seed!(739587835)
+
+function worker()
+    sleep(0.2)
+end
+
+# launch workers
+tasks = [Threads.@spawn worker() for i in 1:Threads.nthreads()]
 println(Random.randn(10))
